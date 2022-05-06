56124d3523bd637fa66559b1da314a47b73cfa42
DanielChamaca <vivalafifa2013@gmail.com>
2022-05-06 10:17:30 -0400

    primer componente

---
+++ archivos/examenparciala/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import jump from './jump.jsx'
+function App() {
+  return (
+    <div className="App">
+      <jump />
+    </div>
+  );
+}
+
+export default App;
---
+++ archivos/examenparciala/src/jump.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import card from "./componentes/tarjeta.jsx"
+
+const jump = ()=>{
+    return(
+        <div className="bloque">
+            <card/>
+        </div>
+    );
+}
+
+export default jump;
